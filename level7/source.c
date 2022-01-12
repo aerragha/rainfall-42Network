e2b03c1c206c8946c57c419d5d2f4e6f2b3ecd97
Erraghay ayoub <erraghayayoub@gmail.com>
2022-01-12 17:45:27 +0100

    fix level7

--- level7/source.c
+++ level7/source.c
@@ -3,6 +3,12 @@
 #include <time.h>
 #include <stdlib.h>
 
+struct internet {
+  int priority;
+  char *name;
+};
+
+
 char c[64];
 
 void m(void)
@@ -12,18 +18,21 @@ void m(void)
 
 int main(int argc, char **argv)
 {
+    struct internet *str1, *str2;
     char *str1;
     char *str2;
     FILE *fs;
 
-    str1 = malloc(8);
-    *str1 = 1;
+    str1 = malloc(sizeof(struct internet));
+    str1->priority = 1;
+    str1->name = malloc(8);
 
-    str2 = malloc(8);
-    *str2 = 2;
+    str2 = malloc(sizeof(struct internet));
+    str2->priority = 2;
+    str2->name = malloc(8);
 
-    strcpy(str1, argv[1]);
-    strcpy(str2, argv[2]);
+    strcpy(str1->name, argv[1]);
+    strcpy(str2->name, argv[2]);
 
     fopen("/home/user/level8/.pass", "r");
     fgets(c, 64, fs);
